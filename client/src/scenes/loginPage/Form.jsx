c131cc27b10d2e7d0619e9e1752a324b80123f99
Mustafalw02 <mustafa.lightwala99@gmail.com>
2023-09-16 13:07:12 +0530

    Updated API endpoints

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -66,7 +66,7 @@ const Form = () => {
     formData.append("picturePath", values.picturePath.name);
 
     const savedUserResponse = await fetch(
-      "http://localhost:3001/auth/register",
+      "https://sociology-service.onrender.com/auth/register",
       {
         method: "POST",
         body: formData,
@@ -82,7 +82,7 @@ const Form = () => {
 
   const login = async (values, onSubmitProps) => {
     const loggedInResponse = await fetch(
-      "http://localhost:3001/auth/login",
+      "https://sociology-service.onrender.com/auth/login",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
